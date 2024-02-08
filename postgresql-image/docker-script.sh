1bac6b93a01b58b2aae8f777cfad5711df4291cf
ines-mgg <i.maganga@yahoo.com>
2024-02-08 12:15:35 +0100

    postgresql image

---
+++ postgresql-image/docker-script.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+    chown -R postgres "$PGDATA"
+    su-exec postgres initdb
+
+    : ${POSTGRES_USER:="postgres"}
+    : ${POSTGRES_DB:=$POSTGRES_USER}
+
+    if [ "$POSTGRES_PASSWORD" ]; then
+      pass="PASSWORD '$POSTGRES_PASSWORD'"
+      authMethod=md5
+    else
+      pass=
+      authMethod=trust
+    fi
+    echo
+
+
+    if [ "$POSTGRES_DB" != 'postgres' ]; then
+      createSql="CREATE DATABASE $POSTGRES_DB;"
+      echo $createSql | su-exec postgres postgres --single -jE
+      echo
+    fi
+
+    if [ "$POSTGRES_USER" != 'postgres' ]; then
+      op=CREATE
+    else
+      op=ALTER
+    fi
+
+    userSql="$op USER $POSTGRES_USER WITH SUPERUSER $pass;"
+    echo $userSql | su-exec postgres postgres --single -jE
+    echo
+
+    { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA"/pg_hba.conf
+    { echo; echo "listen_addresses='*'";} >> "$PGDATA"/postgresql.conf
+
+
+exec su-exec postgres "$@"
