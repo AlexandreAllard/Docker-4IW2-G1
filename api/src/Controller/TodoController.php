99634fa256b2c1e17a288381f1854364cf9f69d6
Victor GRANDIN <78478328+grdnvictor@users.noreply.github.com>
2024-03-04 00:59:56 +0100

    added todos

--- api/src/Controller/TodoController.php
+++ api/src/Controller/TodoController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Schema;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/todos')]
 class TodoController extends AbstractController
 {
-    #[Route('/todos', name: 'todos')]
-    public function get(): Response
+    #[Route('', name: 'show_todos')]
+    public function show(Connection $connection): Response
     {
-        return $this->json([
-            'todo' => 'Récupérer les todos de la base postgres'
+
+        $response = $this->get($connection);
+        $todos = json_decode($response->getContent(), true);
+
+        return $this->render('todo.html.twig', [
+            'todos' => $todos,
         ]);
+    }
+
+    #[Route('/get', name: 'todos')]
+    public function get(Connection $connection): JsonResponse
+    {
+        $sql = 'SELECT * FROM todo';
+        $todos = $connection->fetchAllAssociative($sql);
+
+        return $this->json($todos);
     }
 }
